052d514cc3b05b709a5df6a379e51155d8c87f99
Marcos <marcos.castanedaguzman@hotmail.com>
2019-09-07 10:49:54 -0500

    Versiones finales

--- practica3/actores.js
+++ practica3/actores.js
@@ -8,19 +8,17 @@
 const xml2js = require("xml2js");
 const fs = require("fs");
 const archiveXML = fs.readFileSync("movies.xml","utf8");
-const util = require("util")
 
-var actores = ''
+var actores = '';
 xml2js.parseString(archiveXML,{explicitArray:false},(err,result)=> {
     result.movies.film.forEach((element)=>{
-            actores += '<br>' + element.$.name + '<br>'
-            if (element.cast != undefined){
-                element.cast.forEach((element)=>{
-                    actores += '&nbsp;&nbsp;&nbsp;' + element.trim() + '<br>'
-                }); 
-            }
-            
+        actores += '<br>' + element.$.name + '<br>';
+        if (element.cast != undefined){
+            element.cast.forEach((element)=>{
+                actores += '&nbsp;&nbsp;&nbsp;' + element.trim() + '<br>';
+            }); 
+        }
     });
 });
 
-exports.SimpleMessage = actores
+exports.SimpleMessage = actores;
--- practica3/index.js
+++ practica3/index.js
@@ -1,23 +1,27 @@
-const express = require("express")
-const path = require("path")
-const app = express()
-const view_dir = './view/'
-var peliculas = require('./peliculas.js')
-var actores = require('./actores.js')
+/*----------------------------------------------------------
+ * Práctica 3: Utilizando XML
+ * Fecha: 07-Sep-2019
+ * Autores:
+ *           A01372581 Marcos Eduardo Castañeda Guzmán
+ *           A01169052 Ernestro Cruz López
+ *----------------------------------------------------------*/
+const express = require("express");
+const app = express();
+var peliculas = require('./peliculas.js');
+var actores = require('./actores.js');
 
 app.get('/',(req,res)=>{
-    res.send('Hola al Mundo del Cine')
-})
+    res.send('Hola al Mundo del Cine');
+});
 
 app.get('/peliculas',(req,res)=>{
-    res.send(peliculas.SimpleMessage)
-})
+    res.send(peliculas.SimpleMessage);
+});
 
 app.get('/actores',(req,res)=>{
-    res.send(actores.SimpleMessage)
-})
-
+    res.send(actores.SimpleMessage);
+});
 
 app.listen(process.env.PORT,()=>{
-    console.log('Server on port: ', process.env.PORT)
+    console.log('Server on port: ', process.env.PORT);
 });

0f451fc6a05fb9852c2ae0870238c6048e68dd41
Ernesto <a01169052@itesm.mx>
2019-11-17 23:01:46 +0000

    Crazy8-0.0(Schemas)

--- Crazy8/Server/models/cards.js
+++ Crazy8/Server/models/cards.js
@@ -1,15 +1,15 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-var jugadorSchema = new Schema(
+var JugadorSchema = new Schema(
     {
         _id: mongoose.Schema.Types.ObjectId,
         id : {Number, min:1, max:6},
         nombre: String,
-        cartas:[cartaSchema]
+        cartas:[CartaSchema]
     });
 
-var cartaSchema = new Schema(
+var CartaSchema = new Schema(
     {
         _id: mongoose.Schema.Types.ObjectId,
         id: {type:Number},
@@ -20,10 +20,12 @@ var cartaSchema = new Schema(
 
 var CrazySchema = new Schema({
    juego : {type:Number},
-   jugadores : [jugadorSchema],
-   cartas : [cartaSchema],
-   cartaActual : cartaSchema,
+   jugadores : [JugadorSchema],
+   cartas : [CartaSchema],
+   cartaActual : CartaSchema,
    turno : {type:Number},
 });
 
 module.exports= mongoose.model('Crazy',CrazySchema)
+module.exports= mongoose.model('Jugador',JugadorSchema)
+module.exports= mongoose.model('Carta',CartaSchema)
